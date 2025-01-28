ee528cb227e5ba22616deb7e20e2090566256171
Genesis1821 <castillocaprazog@uvm.edu.ve>
2025-01-27 23:45:17 -0400

    cambios estilos galeria de imagenes

--- coffee_roll/src/paginas/LandingPage.js
+++ coffee_roll/src/paginas/LandingPage.js
@@ -9,6 +9,20 @@ function LandingPage (){
         <>
         <Navbar />
         <div className='contenedor'>
+            <span>
+                <h2>Cafeteria Coffee Rolls</h2>
+                <p>Coffee Rolls está diseñado para que estés cómodo y feliz mientras disfrutas y compartes con tus seres queridos de los productos que tenemos para tí..!</p>
+                <p>Disfruta de un momento agradable en nuestras sedes y endulza tu día. Trabajamos todos los días desde las 9:00am hasta las 9:30pm.¡No te lo puedes perder!</p>
+                <h3>Ubicaciones</h3>
+                <ul>
+                    <li>Merida -Teleferico Mucumbarila</li>
+                    <li>Caracas-CC Sambil Chacao</li>
+                    <li>Caracas-CC sambil candelaria</li>
+                    <li>La Guaira-Aeropuerto de Maiquetía </li>
+                    <li>Caracas- CC Boleita center</li>
+                </ul>
+            </span>
+            
             <GaleriaImg/>
         </div>
         </>
